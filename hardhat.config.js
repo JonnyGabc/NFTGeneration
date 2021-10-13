0dffe4205e73a151f839a414fc0b5402d3870eb4
JonnyGabc <67452359+JonnyGabc@users.noreply.github.com>
2021-10-13 16:08:33 -0400

    set up intial nft system

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { ALCHEMY_URL, METAMASK_PRIVATE_KEY } = process.env;
+module.exports = {
+   solidity: "0.8.0",
+   defaultNetwork: "ropsten",
+   networks: {
+      hardhat: {},
+      ropsten: {
+         url: ALCHEMY_URL,
+         accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+      }
+   },
+}
