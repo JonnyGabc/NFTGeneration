a3a3850b6f5e6847c6b0e7349ae2b35d4ee0c00e
JonnyGabc <67452359+JonnyGabc@users.noreply.github.com>
2021-10-16 17:04:10 -0400

    Transactions succesfully posted to ropsten network

--- contracts/SkinToken.sol
+++ contracts/SkinToken.sol
@@ -15,21 +15,19 @@ contract ExampleNFT is ERC721URIStorage, Ownable {
   Counters.Counter private _tokenIds;
   mapping(string => uint8) hashes;
   // the name and symbol for the NFT
-  constructor() public ERC721("Example", "CLS") {}
+  constructor() ERC721("Example", "CLS") {}
   // Create a function to mint/create the NFT
   // receiver takes a type of address. This is the wallet address of the user that should receive the NFT minted using the smart contract
   // tokenURI takes a string that contains metadata about the NFT
 
-  function createNFT(address receiver, string memory hash, string memory metadata)
+  function createNFT(address receiver, string memory tokenURI)
     public onlyOwner
     returns (uint256)
-  {
-    require(hashes[hash] != 1);
-    hashes[hash] = 1;
+  {    
     _tokenIds.increment();
     uint256 newItemId = _tokenIds.current();
     _mint(receiver, newItemId);
-    _setTokenURI(newItemId, metadata);
+    _setTokenURI(newItemId, tokenURI);
     // returns the id for the newly created token
     return newItemId;
   }
--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -3,3 +3,56 @@ const ALCHEMY_URL = process.env.ALCHEMY_URL;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const alchemyWeb3 = createAlchemyWeb3(ALCHEMY_URL);
 const contract = require("../artifacts/contracts/SkinToken.sol/ExampleNFT.json"); 
+const contractAddress = "0x3130E6040CF6601adceBF737AF327d8B4C8DF13d";
+const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
+
+
+
+const METAMASK_PUBLIC_KEY = process.env.METAMASK_PUBLIC_KEY;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+const walletAddress=METAMASK_PUBLIC_KEY;
+
+async function mintNFT(tokenURI) {
+    // get the nonce - nonce is needed for security reasons. It keeps track of the number of
+    // transactions sent from our address and prevents replay attacks.
+    const nonce = await (alchemyWeb3.eth.getTransactionCount(walletAddress,'latest')+1)
+    const transactionData = {
+    from: METAMASK_PUBLIC_KEY, // our MetaMask public key
+    to: contractAddress, // the smart contract address we want to interact with
+    // nonce: nonce, // nonce with the no of transactions from our account (not working)
+    gas: 1000000, // fee estimate to complete the transaction
+    data: nftContract.methods
+      .createNFT(walletAddress, tokenURI)
+      .encodeABI(), // call the createNFT function from our OsunRiverNFT.sol file and pass the account that should receive the minted NFT.
+  };
+
+  const signPromise = alchemyWeb3.eth.accounts.signTransaction(
+    transactionData,
+    METAMASK_PRIVATE_KEY
+  );
+  signPromise
+    .then((signedTx) => {
+      alchemyWeb3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of our transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of our transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting our transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+}
+
+mintNFT("https://ipfs.io/ipfs/QmayjR57nqdbEUEULu14nWq2rF6M3bzLMrwuNRJArYac8v")
